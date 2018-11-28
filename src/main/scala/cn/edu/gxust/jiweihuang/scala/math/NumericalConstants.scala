2f9d0309e0f52ba56b750c7a5239ef791d83dc47
JiweiHuang <huangjiwei@gxust.edu.cn>
2018-11-28 21:56:22 +0800

    Create Projects

---
+++ src/main/scala/cn/edu/gxust/jiweihuang/scala/math/NumericalConstants.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018-2019, Jiwei Huang. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.edu.gxust.jiweihuang.scala.math
+
+/**
+  * <p>The object {@code NumericalConstants} is used for
+  * representing numerical constants.</p>
+  *
+  * @author JiweiHuang
+  * @version 1.0.0_build-20181128
+  * @see java.io.Serializable
+  */
+object NumericalConstants extends Serializable {
+  /**
+    * The numerical precision for numerical calculation.
+    */
+  val NUMERICAL_PRECISION = 1.0E-15
+}
